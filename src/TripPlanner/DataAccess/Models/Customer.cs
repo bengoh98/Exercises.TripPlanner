8a9a83074c37c2cb77ab979b55bb680b29ccdacd
Benjamin Goh <benjamingoh98@gmail.com>
2016-08-11 13:15:45 +1000

    Added initial data access models

---
+++ src/TripPlanner/DataAccess/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripPlanner.DataAccess.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string StreetAddress { get; set; }
+        public string Suburb { get; set; }
+        public string State { get; set; }
+        public string Postcode { get; set; }
+    }
+}
---
+++ src/TripPlanner/DataAccess/Models/Package.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripPlanner.DataAccess.Models
+{
+    public class Package
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
